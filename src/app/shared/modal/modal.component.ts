96659d4b694ffc3f6f4f38de646a95604f0fc18d
ahamedzoha <ahamed.zoha@gmail.com>
2023-04-13 02:39:26 +0600

    created a reusable modal component inside the shared module

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent {
+  isVisible: boolean = false;
 
+  constructor() {
+    this.isVisible = true;
+  }
+
+  toggleModal() {
+    this.isVisible = !this.isVisible;
+  }
 }
